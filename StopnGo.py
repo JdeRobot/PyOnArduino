ad40d2f874c30ffcb7ae9adcfbfbce30cd517308
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-05-24 11:34:32 +0200

    Stop & Go basic functionality that should match the actual Arduino behaviour

---
+++ StopnGo.py
@@ -0,0 +1,21 @@
+def get_US() -> int:
+    '''
+        Instead of reading the distance from keyboard we should get the measurement from the actual ultrasonic
+        sensor
+    '''
+    return int(input('Distance: '))
+
+def set_engine(direction):
+    '''
+        Instead of just printing the direction the engines will have, we should actually set it within this function
+    '''
+    if direction == 0:
+        print('STOP! ')
+    elif direction == 1:
+        print('Forward')
+
+while True:
+    if get_US() < 10:
+        set_engine(0)
+    else:
+        set_engine(1)
