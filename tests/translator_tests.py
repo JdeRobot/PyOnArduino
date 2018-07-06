5145a3812e68606633cc8e79e541336767521796
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-07-06 11:33:40 +0200

    SetUp method override in the tests file

--- tests/translator_tests.py
+++ tests/translator_tests.py
@@ -3,17 +3,18 @@ import unittest
 import translator.Translator as translator
 
 class MyTest(unittest.TestCase):
-    def test_print_hello_world(self):
+    def setUp(self):
+        global visitor
         translator.function_def = ''
         visitor = translator.MyVisitor()
+
+    def test_print_hello_world(self):
         parsed_statement = ast.parse('print(\'Hello World!\')')
         visitor.visit(parsed_statement)
         expected_statement = 'Serial.print("Hello World!");\n   '
         self.assertEqual(expected_statement, translator.function_def)
 
     def test_sleep(self):
-        translator.function_def = ''
-        visitor = translator.MyVisitor()
         parsed_statement = ast.parse('sleep(100)')
         visitor.visit(parsed_statement)
         expected_statement = 'delay(100);\n   '
