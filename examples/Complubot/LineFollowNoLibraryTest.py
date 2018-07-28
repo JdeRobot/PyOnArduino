b24c894706a4b85a32c5d1198775e28f5a03670d
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-07-28 12:54:11 +0200

    Complubot LineFollow example updated

--- examples/Complubot/LineFollowNoLibraryTest.py
+++ examples/Complubot/LineFollowNoLibraryTest.py
@@ -2,7 +2,9 @@ import HALduino.halduino as halduino
 
 
 def loop():
-    if halduino.getIR2() < halduino.getIR4() and halduino.getIR5() < 990:
-        halduino.setSpeedEnginesMotor(0, 110)
-    else:
+    if halduino.getIR3() < 300:
+        halduino.setSpeedEnginesMotor(110, 110)
+    if halduino.getIR2() < 300:
         halduino.setSpeedEnginesMotor(110, 0)
+    if halduino.getIR4() < 300:
+        halduino.setSpeedEnginesMotor(0, 110)
